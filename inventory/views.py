62b2d4ba6a06d19aa3623489746cbaf8ff94edb6
lauren brett <laurencareybrett@gmail.com>
2020-06-11 11:41:12 +0100

    index page response

--- inventory/views.py
+++ inventory/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello from the inventory index")
+    # return HttpResponse("Hello from the inventory index")
+    return render(request, "inventory/index.html")
