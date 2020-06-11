f36d69476b0c756cedf98ee2c4b7418c013dad84
lauren brett <laurencareybrett@gmail.com>
2020-06-11 11:03:07 +0100

    admin

--- inventory/models.py
+++ inventory/models.py
@@ -6,10 +6,14 @@ class Artist(models.Model):
     #-- django mapping as varchar to table
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Album(models.Model):
     title = models.CharField(max_length=255)
     year = models.IntegerField()
     stock_level = models.IntegerField()
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
-    
+    def __str__(self):
+        return self.title
